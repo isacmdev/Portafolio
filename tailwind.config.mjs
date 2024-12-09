a5b8f2426911e6b73a6e29aad1fbc8323a96a2cb
Isabel Caballero <isabelcaballeromotta@hotmail.com>
2024-12-09 10:50:46 -0500

    first commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+			  softRed: '#FFABAB',
+			  darkPink: '#FCC8D1',
+			
+			},
+		},	
 	},
 	plugins: [],
 }
